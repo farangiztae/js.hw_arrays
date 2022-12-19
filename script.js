a2f94cfcd7646847395f582ae9e888ac684c1e85
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2022-12-19 15:04:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//push
+// let fruits = ["Яблоко", "Апельсин", "Слива"];
+// fruits.splice(5, 0, "Лимон");
+// console.log(fruits);
+
+//pop
+// let fruits = ["Яблоко", "Апельсин", "Слива"];
+// let last = fruits.length - 1;
+// fruits.splice(last);
+// console.log(fruits);
